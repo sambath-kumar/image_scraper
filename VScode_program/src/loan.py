c87b103a1a2d5556ca70bbbc8be76e350fba7e92
Sambath Kumar <64127985+sambath-kumar@users.noreply.github.com>
2023-08-01 11:50:05 +0530

    Delete VScode_program directory

--- VScode_program/src/loan.py
+++
@@ -1,9 +0,0 @@
-class Loan:
-    def __init__(self, acc, name, existing_loan, cibil):
-        self.acc = acc
-        self.name = name
-        self.existing_loan = existing_loan
-        self.cibil = cibil
-    def status(self):
-        print(f"The account holder {self.name} with account number {self.acc}")
-        print(f"He is loan status ={self.existing_loan} and cibil score is {self.cibil}")
--- VScode_program/src/vehicle.py
+++
@@ -1,7 +0,0 @@
-class Vehicle:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
-        
-    def disp(self):
-        print(f"The brand is {self.brand} and model is {self.model}")
